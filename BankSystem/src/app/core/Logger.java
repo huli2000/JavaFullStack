6e59d99a8d29613ed02c1f0c9757c591b4679623
Itamar Huli <huli2000@gmail.com>
2021-06-29 09:37:38 +0300

    bank

---
+++ BankSystem/src/app/core/Logger.java
@@ -0,0 +1,18 @@
+package app.core;
+
+public class Logger {
+
+	private String driverName;
+
+	public Logger(String driverName) {
+		super();
+		this.driverName = driverName;
+	}
+	
+	public void log (Log log) {
+		System.out.println(log.getData());
+	}
+		
+	
+	
+}
