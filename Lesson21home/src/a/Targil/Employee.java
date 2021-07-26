48a8c3581c7af6cd26a1510c66864450cc0606ca
huli2000 <huli2000@HP9470Huli>
2021-07-26 19:08:23 +0300

    Signed-off-by: huli2000 <huli2000@HP9470Huli>

--- Lesson21home/src/a/Targil/Employee.java
+++ Lesson21home/src/a/Targil/Employee.java
@@ -7,4 +7,62 @@ public class Employee {
 	private double salary;
 	private String department;
 	
+	public Employee () {
+		
+	}
+
+	public Employee(String first, String last, double salary, String department) {
+		super();
+		this.first = first;
+		this.last = last;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+//	public double getSalary() {
+//		return salary;
+//	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [first=" + first + ", last=" + last + ", salary=" + salary + ", department=" + department
+				+ "]";
+	}
+
+	public double getSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	
 }
